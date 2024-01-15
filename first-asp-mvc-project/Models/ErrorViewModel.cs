97c5aa33a81032629be0d1b0b5e665d3b38312eb
Juwel <developerjuwel792@gmail.com>
2024-01-16 00:38:34 +0600

    Add project files.

---
+++ first-asp-mvc-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace first_asp_mvc_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
